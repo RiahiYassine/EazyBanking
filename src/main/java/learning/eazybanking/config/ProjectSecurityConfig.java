e3e4e2c3c83d788fc2d30cdeff71a1d1bc635741
Yassine RIAHI <yassineriahi0417@gmail.com>
2024-11-10 13:52:11 +0100

    Configuring PasswordEnCoder using PasswordEncoderFactories

--- src/main/java/learning/eazybanking/config/ProjectSecurityConfig.java
+++ src/main/java/learning/eazybanking/config/ProjectSecurityConfig.java
@@ -3,10 +3,11 @@ package learning.eazybanking.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,9 +33,16 @@ public class ProjectSecurityConfig{
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user").password("{noop}123456").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}123456").authorities("admin").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$5gQ3D7fMe6TnpyoZ5T2tG.59fL/.JQJloA6zq6xYbmEzuHWZ8hk1a").authorities("admin").build(); //bcrypt-generated.com
         return new InMemoryUserDetailsManager(user, admin);
 
     }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 
 }
