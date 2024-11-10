2e35a3700e161705db9fe45050b2b2f0b81f07ac
Yassine RIAHI <yassineriahi0417@gmail.com>
2024-11-10 12:56:46 +0100

    Configuring users using InMemoryUserDetailsManager

--- src/main/java/learning/eazybanking/config/ProjectSecurityConfig.java
+++ src/main/java/learning/eazybanking/config/ProjectSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -23,6 +27,14 @@ public class ProjectSecurityConfig{
         http.httpBasic(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("user").password("{noop}123456").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}123456").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+
     }
 
 }
