1c97788b056cadb5544afc4102ffd83fbefc40ef
Yassine RIAHI <yassineriahi0417@gmail.com>
2024-11-10 11:20:50 +0100

    Initial commit

---
+++ src/main/java/learning/eazybanking/EazyBankingApplication.java
@@ -0,0 +1,13 @@
+package learning.eazybanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazyBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/learning/eazybanking/EazyBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.eazybanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
